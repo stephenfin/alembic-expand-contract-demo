7d18c92ec47233ec03d2fe49a1e2e093d9c6aa07
Stephen Finucane <stephen@that.guru>
2022-11-09 17:34:04 +0000

    Remove manually generated migrations
    In preparation for the auto-generated migrations.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- alembic_expand_contract_demo/migrations/versions/9c36df1b3f62_initial_expand.py
+++ alembic_expand_contract_demo/migrations/versions/9c36df1b3f62_initial_expand.py
@@ -1,11 +1,8 @@
-"""Merge user names (expand)
+"""Initial expand migration
 
 Revision ID: 9c36df1b3f62
 Revises: 6cb93d555e2b
 """
-
-from alembic import op
-import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '9c36df1b3f62'
@@ -15,7 +12,4 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column(
-        'user_account',
-        sa.Column('name', sa.String(), nullable=True),
-    )
+    pass
---
+++ alembic_expand_contract_demo/migrations/versions/e629a4ea0677_initial_contract.py
@@ -0,0 +1,15 @@
+"""Initial contract migration
+
+Revision ID: e629a4ea0677
+Revises: 6cb93d555e2b
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'e629a4ea0677'
+down_revision = '6cb93d555e2b'
+branch_labels = ('contract',)
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
